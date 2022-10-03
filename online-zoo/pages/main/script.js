5b27c3fe1e0126456463e4b478abab42f31f0c61
A. Shustovsky <a.shustovsky@gmail.com>
2022-10-03 18:03:14 +0400

    feat: add burger menu to main and donation pages

---
+++ online-zoo/pages/main/script.js
@@ -0,0 +1,18 @@
+const burger = document.querySelector('.burger');
+const burgerMenu = document.querySelector('.burger_menu');
+const wrapper = document.querySelector('.wrapper');
+
+burger.addEventListener('click', () => {
+    burger.classList.toggle('burger-active');
+    burgerMenu.classList.toggle('burger_menu-active');
+})
+
+burgerMenu.addEventListener('click', (e) => {
+    const withinWrapper = e.composedPath().includes(wrapper);
+
+    if (!withinWrapper) {
+        console.log('!!!!!!!!!!')
+        burger.classList.toggle('burger-active');
+        burgerMenu.classList.toggle('burger_menu-active');
+    }
+})
