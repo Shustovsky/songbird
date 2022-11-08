b2d532edfdeb705db6fef1432d747a6908bea4a2
A. Shustovsky <a.shustovsky@gmail.com>
2022-11-08 13:51:03 +0400

    init: create structure of project and add header with adaptation to main page

---
+++ songbird/pages/main/script.js
@@ -0,0 +1,17 @@
+const burger = document.querySelector('.burger');
+const burgerMenu = document.querySelector('.burger_menu');
+const wrapper = document.querySelector('.wrapper');
+
+burger.addEventListener('click', () => {
+  burger.classList.toggle('burger-active');
+  burgerMenu.classList.toggle('burger_menu-active');
+})
+
+burgerMenu.addEventListener('click', (e) => {
+  const withinWrapper = e.composedPath().includes(wrapper);
+
+  if (!withinWrapper) {
+    burger.classList.toggle('burger-active');
+    burgerMenu.classList.toggle('burger_menu-active');
+  }
+})
