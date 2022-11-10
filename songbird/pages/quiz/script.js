2b716d9a2a920587bba5c090d42761c1acd77500
A. Shustovsky <a.shustovsky@gmail.com>
2022-11-10 14:05:29 +0400

    feat: add hideInfo and showInfo func and refactor code

--- songbird/pages/quiz/script.js
+++ songbird/pages/quiz/script.js
@@ -24,28 +24,35 @@ let stageNumb = 0;
 let randomNum;
 let haveTrueAnswer = false;
 
-const btn = document.querySelector('.questions__btn');
-btn.addEventListener('click', () => {
-  if (haveTrueAnswer) {
-    stageNumb++;
-    haveTrueAnswer = false;
-    selectActiveNavItem();
-    hideName();
-    hideImg();
-    fillAnswerItems();
-    console.log(`true bird === ${birdsData[stageNumb][randomNum].name}`);
-  };
-});
-
 function getStarted() {
   hideName();
   hideImg();
+  hideInfo();
   getRandom();
   fillAnswerItems();
   checkTrueAnswer();
   console.log(`true bird === ${birdsData[stageNumb][randomNum].name}`);
 }
 getStarted();
+
+function pushBtn() {
+  const btn = document.querySelector('.questions__btn');
+  btn.addEventListener('click', () => {
+    if (haveTrueAnswer) {
+      stageNumb++;
+      haveTrueAnswer = false;
+      selectActiveNavItem();
+      hideName();
+      hideImg();
+      hideInfo();
+      getRandom();
+      fillAnswerItems();
+      checkTrueAnswer();
+      console.log(`true bird === ${birdsData[stageNumb][randomNum].name}`);
+    };
+  });
+};
+pushBtn();
 
 function selectActiveNavItem() {
   const navItems = document.querySelectorAll('.questions__list_item');
@@ -76,14 +83,24 @@ function fillAnswerItems() {
 function checkTrueAnswer() {
   const items = document.querySelectorAll('.questions__answer_item');
   items.forEach(item => {
+
+    const name = birdsData[stageNumb][randomNum].name;
+    const species = birdsData[stageNumb][randomNum].species;
+    const description = birdsData[stageNumb][randomNum].description;
+    const image = birdsData[stageNumb][randomNum].image;
+    const audio = birdsData[stageNumb][randomNum].audio;
+
     item.addEventListener('click', () => {
-      if (item.innerHTML === birdsData[stageNumb][randomNum].name) {
-        item.classList.add('questions__answer_item-right');
-        showName(birdsData[stageNumb][randomNum].name);
-        showImg(birdsData[stageNumb][randomNum].image, birdsData[stageNumb][randomNum].name);
-        haveTrueAnswer = true;
-      } else {
-        item.classList.add('questions__answer_item-wrong');
+      if (haveTrueAnswer === false) {
+        if (item.innerHTML === name) {
+          item.classList.add('questions__answer_item-right');
+          showName(name);
+          showImg(image, name);
+          showInfo(name, species, description, image);
+          haveTrueAnswer = true;
+        } else {
+          item.classList.add('questions__answer_item-wrong');
+        };
       };
     });
   });
@@ -109,4 +126,30 @@ function showImg(path, alt) {
   const img = document.querySelector('.questions__current_img img');
   img.src = `${path}`;
   img.alt = `${alt}`;
+};
+
+function hideInfo() {
+  const helpSection = document.querySelector('.help');
+  const descriptionSection = document.querySelector('.description');
+  helpSection.style.display = 'block';
+  descriptionSection.style.display = 'none';
+}
+// hideInfo()
+
+function showInfo(name, species, description, image) {
+  const helpSection = document.querySelector('.help');
+  const descriptionSection = document.querySelector('.description');
+  helpSection.style.display = 'none';
+  descriptionSection.style.display = 'inline-grid';
+
+  const imgItem = document.querySelector('.description_img img');
+  const nameItem = document.querySelector('.description_name');
+  const speciesItem = document.querySelector('.description_species');
+  const dscrItem = document.querySelector('.description_dscr');
+
+  imgItem.src = `${image}`;
+  imgItem.alt = `${name}`;
+  nameItem.innerHTML = `${name}`;
+  speciesItem.innerHTML = `${species}`;
+  dscrItem.innerHTML = `${description}`;
 };
