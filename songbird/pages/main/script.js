75edf01ee48e73c078545dc42bbed8170f82db3c
A. Shustovsky <a.shustovsky@gmail.com>
2022-11-15 12:22:09 +0400

    refactor: add missed semicolons

--- songbird/pages/main/script.js
+++ songbird/pages/main/script.js
@@ -5,7 +5,7 @@ const wrapper = document.querySelector('.wrapper');
 burger.addEventListener('click', () => {
   burger.classList.toggle('burger-active');
   burgerMenu.classList.toggle('burger_menu-active');
-})
+});
 
 burgerMenu.addEventListener('click', (e) => {
   const withinWrapper = e.composedPath().includes(wrapper);
@@ -13,12 +13,10 @@ burgerMenu.addEventListener('click', (e) => {
   if (!withinWrapper) {
     burger.classList.toggle('burger-active');
     burgerMenu.classList.toggle('burger_menu-active');
-  }
-})
+  };
+});
 
 const btn = document.getElementById('btn');
 btn.addEventListener('click', () => {
   document.location = '../quiz/index.html';
-})
-
-{ /* <button onclick="document.location='page/new.html'">Переход по ссылке</button> */ }
+});
