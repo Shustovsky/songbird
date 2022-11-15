fd3cb4d42f801050fce6856b4c3d9e598befdd86
A. Shustovsky <a.shustovsky@gmail.com>
2022-11-15 12:41:06 +0400

    feat: added logic and points display

--- songbird/pages/result/script.js
+++ songbird/pages/result/script.js
@@ -19,15 +19,36 @@ burgerMenu.addEventListener('click', (e) => {
 const btn = document.getElementById('btn');
 btn.addEventListener('click', () => {
   document.location = '../quiz/index.html';
-})
+});
+///////////////////////////////////////////////////////////////////////
 
+let totalResult = 0;
 
-window.addEventListener('load', getLocalStorage);
+window.addEventListener('load', () => {
+  getLocalStorage();
+  if (totalResult === 30) {
+    ifWon();
+  } else {
+    fillResult();
+    console.log(totalResult);
+  };
+});
 
 function getLocalStorage() {
   if (localStorage.getItem('totalScore')) {
-    let abc = localStorage.getItem('totalScore')
-    console.log(abc);
-  }
+    totalResult = +localStorage.getItem('totalScore');
+  };
+};
 
-}
+function fillResult() {
+  const resultSpan = document.getElementById('result');
+  resultSpan.innerHTML = totalResult;
+};
+
+function ifWon() {
+  const btn = document.getElementById('btn');
+  const subtitle = document.querySelector('.subtitle');
+
+  btn.style.display = 'none';
+  subtitle.innerHTML = 'Вы прошли викторину и набрали 30 из 30 возможных баллов';
+};
