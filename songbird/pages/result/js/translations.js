a9ceed62e2dd0479e1f9a1d6f2d9513d694960ae
A. Shustovsky <a.shustovsky@gmail.com>
2022-11-16 17:33:33 +0400

    feat: add ttranslation file for result page

---
+++ songbird/pages/result/js/translations.js
@@ -0,0 +1,29 @@
+export const translations = {
+  ['en']: {
+    langBtn: 'RU',
+    linkToMain: 'Main',
+    linkToQuiz: 'Quiz',
+    linkToGallery: 'Gallery',
+    linkToResult: 'Result',
+    title: 'Congratulate!',
+    subtitlePart1: 'You have passed the quiz and scored ',
+    subtitlePart2: ' out of 30 possible points!',
+    subtitle: '',
+    btnTry: 'Try again!',
+
+
+
+
+  },
+  ['ru']: {
+    langBtn: 'ENG',
+    linkToMain: 'Главная',
+    linkToQuiz: 'Викторина',
+    linkToGallery: 'Галерея',
+    linkToResult: 'Результат',
+    title: 'Поздравляем!',
+    subtitlePart1: 'Вы прошли викторину и набрали ',
+    subtitlePart2: ' из 30 возможных баллов!',
+    btnTry: 'Попробовать еще раз!',
+  },
+};
